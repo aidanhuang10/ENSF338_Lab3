a9408983f0b5cdb2ccb3a6a285b3c6cb5a30d30a
Aidan <aidanhuang10@gmail.com>
2024-02-09 11:51:03 -0700

    Finished ex7

--- ex7.py
+++ ex7.py
@@ -1,21 +1,29 @@
-import sys 
-sys.setrecursionlimit(20000)
 import timeit
-import random 
-import numpy as np
 from matplotlib import pyplot as plt
-import scipy
 import json
-#Binary Search
-def binarySearch (arr, first, last, initial, key):
+
+#Selected Midpoint Binary Search
+def selectedMidPointBinarySearch (arr, first, last, selected_mid, key):
     if (first <= last):
-        mid = initial
+        mid = selected_mid
         if(key == arr[mid]):
             return mid
         elif(key < arr[mid]):
-            return binarySearch(arr, first, mid - 1, (mid // 2), key)
+            return selectedMidPointBinarySearch(arr, first, mid - 1, (first + mid - 1) // 2, key)
         elif(key > arr[mid]):
-            return binarySearch(arr, mid + 1, last, (mid // 2), key)
+            return selectedMidPointBinarySearch(arr, mid + 1, last, (mid + 1 + last) // 2, key)
+    return -1 #Key was not found in the array
+
+#Regular Binary Search
+def BinarySearch (arr, first, last, key):
+    if (first <= last):
+        mid = (first + last) // 2
+        if(key == arr[mid]):
+            return mid
+        elif(key < arr[mid]):
+            return BinarySearch(arr, first, mid - 1, key)
+        elif(key > arr[mid]):
+            return BinarySearch(arr, mid + 1, last, key)
     return -1 #Key was not found in the array
 
 # def percentiles():
@@ -31,18 +39,24 @@ with open('./ENSF338_Lab3/ex7tasks.json', 'r') as f:
 with open('./ENSF338_Lab3/ex7data.json', 'r') as g:
     data = json.load(g)
 
-avg_key_time = []
+avg_selected_time = []
+avg_regular_time = []
 for target in tasks:
-    time = timeit.timeit(lambda: binarySearch(data, 0, len(data) - 1, target, target))
-    avg_key_time.append(time)
+    #Uses the key as the mid point of division  
+    selectedMiddleTime = timeit.timeit(lambda: selectedMidPointBinarySearch(data, 0, len(data) - 1, target, target), number=1)
+    regularTime = timeit.timeit(lambda: BinarySearch(data, 0, len(data) - 1, target), number=1)
+    avg_selected_time.append(selectedMiddleTime)
+    avg_regular_time.append(regularTime)
+    
+# Plotting
+plt.figure(figsize=(8, 6))
+plt.scatter(tasks, avg_selected_time, label='Selected Middle Binary Search', marker='o', color='blue')
+plt.scatter(tasks, avg_regular_time, label='Regular Binary Search', marker='o', color='red')
+plt.xlabel('Target Value')
+plt.ylabel('Average Time (s)')
+plt.title('Average Search Time for Each Target using Binary Search Regular VS Selected Mid Point')
+plt.grid(True)
+plt.legend()
+plt.show()
 
-# avg_time = []
-# for target in tasks:
-#     time = timeit.timeit(lambda: binarySearch(data, 0, len(data) - 1, (len(data) - 1) // 2))
-#     avg_time.append(time)
-
-# plt.scatter(data, avg_time)
-# plt.plot()
-# plt.scatter(data, avg_key_time)
-# plt.show()
     
