c6b8bf37095c57c11cb31940f487739288d5bf54
Rohan Lange <rohan.lange@ucalgary.ca>
2024-02-12 16:07:01 -0700

    added comments and fixed pdf

--- ex2.py
+++ ex2.py
@@ -1,37 +1,54 @@
 import timeit
 import random
 from matplotlib import pyplot as plt
+#create bubble sort function
 def bubbleSort(a,len):
+    #runs once for every element in the array - 1
     for y in range(len-1):
         swaps = False
+        #runs once for every element in the array minus the number of outside loop runs
         for x in range (len-1-y):
+            #if current element is greater than next element swap them
             if a[x] > a[x+1]:
                 swaps = True
                 (a[x],a[x+1]) = (a[x+1],a[x])
+        #if no swaps were made this run then the array is sorted
         if swaps == False:
             break
+#create partition function
 def partition(a,low,high):
+    #set pivot as last element
     pivot = a[high]
+    #set current index as 1 before first element
     i = low-1
+    #runs through the entire range of numbers
     for x in range(low,high):
+        #if current examined number is less than or equal to the pivot
         if a[x] <= pivot:
+            #add it into the next slot on the left side
             i = i+1
             (a[i],a[x]) = (a[x], a[i])
+    #once necesesarry moves have been made put the pivot directly affter all the numbers that are smaller than it
     (a[i+1],a[high]) = (a[high],a[i+1])
+    #return pivot location
     return i+1
+#define quicksort function
 def quickSort(a,low,high):
+    #effectivly, if the array is not size 1
     if low<high:
+        #partition the list and then recursivly call quick sort on each side of the partition
         part = partition(a,low,high)
         quickSort(a,low,part-1)
         quickSort(a,part+1,high)
 #sizes of choice: 2,4,6,8,10,12,14,16,18,20,25,30,35,40,45,50,75,100,125,150
+#create lists to hold results
 bestBubble = []
 worstBubble = []
 averageBubble = []
 worstQuick = []
 averageQuick = []
 choices = [2,4,6,8,10,12,14,16,18,20,25,30,35,40,45,50,75,100,125,150]
-for x in range(20):
+for x in range(20):#run once for each choice
     test1 = [y for y in range(choices[x])]#bubble best
     bestBubble.append(timeit.timeit(lambda: bubbleSort(test1,len(test1)), number=1))
     test1 = [choices[x] - y for y in range(choices[x])]#bubble worse case
@@ -42,6 +59,7 @@ for x in range(20):
     test2 = random.sample(range(0,1000),choices[x])#bubble average, quick average, and quick best
     averageBubble.append(timeit.timeit(lambda: bubbleSort(test1,len(test1)), number=1))
     averageQuick.append(timeit.timeit(lambda: quickSort(test2,0,len(test2)-1), number=1))
+#plot results 
 plt.scatter(choices,bestBubble,color = 'blue')
 plt.scatter(choices,averageQuick,color = 'red')
 plt.xlabel("length of array sorted")
--- ex3.py
+++ ex3.py
@@ -5,42 +5,58 @@ import random
 from matplotlib import pyplot as plt
 import numpy as np
 import scipy
+#define quadratic function for curve fitting
 def func(x, a, b):
     return a * np.square(x) + b
+#define bubble sort with comparison and swap counters
 def bubbleSort(a,len):
     swaps = 0
     comparisons = 0
+    #runs once for each element in the array minus 1
     for y in range(len-1):
         swap = False
+        #runs once for each element in the array minus the number of times the outside loop has ran minus 1
         for x in range (len-1-y):
             comparisons = comparisons + 1 
+            #if current element is bigger than the next element swap them
             if a[x] > a[x+1]:
                 swap = True
                 swaps = swaps+1
                 (a[x],a[x+1]) = (a[x+1],a[x])
+        #if no swaps were made then array is sorted
         if swap == False:
             break
+        #return the number of swaps and comparisons made
     return[swaps,comparisons]
+#define choices for array size and total swaps and comparisons
 choices = [5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100]
 finalSwaps = []
 finalComparisons = []
+#for each array length that was decided upon
 for x in choices:
     swaps = 0
     comparisons = 0
+    #run 100 times
     for y in range(100):
+        #test is an array of x elements with numbers from 0 to 1000
         test = random.sample(range(0,1000),x)
+        #get the number of comparisons made on this run
         result1,result2 = bubbleSort(test,x)
         swaps = swaps + result1
         comparisons = comparisons + result2
+    #devide swaps and comparisons by 100 to get the average
     swaps = swaps/100
     comparisons = comparisons/100
+    #append results to final arrays
     finalSwaps.append(swaps)
     finalComparisons.append(comparisons)
+#create curves to model results
 popt, pcov = scipy.optimize.curve_fit(func, choices, finalComparisons)
 popt2, pcov2 = scipy.optimize.curve_fit(func, choices, finalSwaps)
 x_values = np.linspace(min(choices), max(choices), 100)
 fitted_curve = func(x_values, *popt)
 fitted_curve2 = func(x_values, *popt2)
+#plot results with modeled curves
 plt.scatter(choices,finalComparisons)
 plt.plot(x_values, fitted_curve)
 plt.xlabel("Number of Elements")
